6e1ffd640318e4de5b54bd7c7da707508e850b3d
Margaret mwangi <maggiemwas91@gmail.com>
2021-08-31 15:09:47 +0300

    Updates

---
+++ precache-manifest.30e04afc4736508c39e065beff0f53d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a221baf645558c69156beecf1ae89ba1",
+    "url": "/chuckNorris/index.html"
+  },
+  {
+    "revision": "942466930c88ca6fc2b3",
+    "url": "/chuckNorris/static/css/main.a6e83da7.chunk.css"
+  },
+  {
+    "revision": "4b8537ec8769610ec92d",
+    "url": "/chuckNorris/static/js/2.68f2b285.chunk.js"
+  },
+  {
+    "revision": "452512fa9f48054be807e37947522c62",
+    "url": "/chuckNorris/static/js/2.68f2b285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942466930c88ca6fc2b3",
+    "url": "/chuckNorris/static/js/main.f1d960dc.chunk.js"
+  },
+  {
+    "revision": "bd7c459bbec5db502106",
+    "url": "/chuckNorris/static/js/runtime-main.838bbee4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chuckNorris/static/media/logo.5d5d9eef.svg"
+  }
+]);
